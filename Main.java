e39cecb5b9045b46701129bffe36341062796fca
Liquidwork <52688110+Liquidwork@users.noreply.github.com>
2021-03-30 15:45:07 +0800

    Add title to the main view

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
+import java.awt.Font;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main extends JFrame{
 
@@ -6,10 +8,16 @@ public class Main extends JFrame{
 
 
     public Main(){
+        this.setTitle("Digital Gym System");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
         this.setSize(800,500);
         this.setLocation(400, 100);
         this.setVisible(true);
+        JLabel title = new JLabel("Digital Gym", JLabel.CENTER);
+        title.setBounds(200, 100, 400, 200);
+        title.setFont(new Font("Arial", Font.BOLD, 48));
+        this.add(title);
     }
 
     //Method to run the program
