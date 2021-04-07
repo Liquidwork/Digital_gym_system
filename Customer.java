ca8aa0431a67810812bc52b3d05db0aa0bb9f311
SiegmeyerG <81668609+SiegmeyerG@users.noreply.github.com>
2021-04-07 18:39:03 +0800

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,18 +1,16 @@
 import java.util.ArrayList;
 
 public class Customer extends User {
-    /**
-     * This is the constructor of a customer. Only extra param will be listed
-     * Only has new setter and getter for money
-     * @param money  store for the balance
-     * @param trainers future used for teachers
-     * @param favouratevideos future used for favors
-     */
     private double money;
     //private String[] trainers;                    //future
     //private ArrayList<Integer> favouratevideos;   //future
     //private List<ExerciseRecord> exerciseRecordList;  //to do
-
+    
+    /**
+     * This is the constructor of a customer extending user. Only extra param will be listed
+     * Only has new setter and getter methods for money
+     * @param money  stored information for the balance
+     */
     public Customer(int id, String name, Type type, double money) {
         super(id, name, type);
         this.money = money;
