fff3b0d52caa9c2e24af1e47c9ca577727ae348f
landeqi-te <1078259513@qq.com>
2021-04-10 14:26:44 +0800

    Update LoginController.java

--- LoginController.java
+++ LoginController.java
@@ -1,16 +1,16 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
- * A login controller which provide methods to access user's login data 
+ * A login controller which provide methods to access user's login data
  */
 public class LoginController {
-    private static final String loginPathname = "./data/login.csv"; // file path of login data
+    private static final String loginPathname =  "./data/login.csv"; // file path of login data
     private static final String cashPathname = "./data/cash.csv"; // file path of cash
     private static final ArrayList<String> idlist=new ArrayList<>();
     private static final ArrayList<String> namelist= new ArrayList<String>();
     private  static  final ArrayList<String> passwordlist= new ArrayList<String>();
+    private static final ArrayList<String> typelist= new ArrayList<>();
 
     /**
      * Login to an account, initiate and return the customer if success, return null if login failed.
@@ -20,7 +20,7 @@ public class LoginController {
      * @return the user if login success, null if login failed
      * @seeUser
      */
-    public static User login(String name, String password){
+    public static User login(String name, String password) throws MyException {
         // TODO: Read a csv to check if user's login data correct, return user if login successfully, otherwise null will be returned
 
         File csv=new File(loginPathname);
@@ -34,38 +34,35 @@ public class LoginController {
             e.printStackTrace();
         }
         String line=null;
-        String line1=null;
         int flag=0;
         int i=1;
         try {
             while ((line= br.readLine())!=null) {//read each line of File login
                 String item[]=line.split(",");
-                idlist.add(item[0]);
-                namelist.add(item[1]);
-                passwordlist.add(item[2]);
-                if((name.equals(item[1]))&&(password.equals(item[2]))){ //compare the argument name and password with File csv and if the user is a member
-                    flag=1;
-                    User.Type type=User.Type.valueOf(item[3]);
-                    double money=0.00;
-                    while ((line1= br1.readLine())!=null){ //read each line of File cash
-                        String mon[]=line1.split(",");
-                        if (String.valueOf(i).equals(mon[0])){
-                            money=Double.parseDouble(mon[1]);
-                        }
+                typelist.add(item[3]);
+                if((name.equals(item[1]))&&(password.equals(item[2]))) { //compare the argument name and password with File csv and if the user is a member
+                    //return diffirent types of users
+                    if (typelist.get(i - 1).equals("customer")) {
+                        Customer customer = new Customer(i, item[1]);// create a customer and return it
+                        return customer;
+                    } else if (typelist.get(i - 1).equals("admin")) {
+                        Admin admin = new Admin(i, item[1]);
+                        return admin;
+                    } else {
+                        Trainer trainer = new Trainer(i, item[1]);
+                        return trainer;
                     }
-                    System.out.println("Login successfully!");
-                    Customer customer=new Customer(i,item[1],type,money);// create a customer and return it
-                    return customer;
                 }
                 i+=1;
             }
-            if(flag==0){// the user is not a member
-                System.out.println("Sorry! You're not a member yet. \n"+"Please register!");
+            if(flag==0){// the user is not a member  and throw a exception
+                MyException myException=new MyException("You are not a member");
+                throw myException;
             }
         }catch (IOException e) {
             e.printStackTrace();
         }
-        // System.out.println(namelist);
+
 
         return null;
     }
@@ -79,7 +76,7 @@ public class LoginController {
      * @return the user if login success, null if login failed
      * @seeUser
      */
-    public static Customer register(String name, String password, User.Type type) {
+    public static User register(String name, String password, String type) throws MyException {
         // TODO: Read a csv to check if user's register data valid, return user if register successfully, otherwise null will be returned
         File csv=new File(loginPathname);
         File cash=new File(cashPathname);
@@ -93,12 +90,14 @@ public class LoginController {
         idlist.clear();// clear the arraylist
         namelist.clear();
         passwordlist.clear();
+        typelist.clear();
         try{
             while ((line= br.readLine())!=null){
                 String item[]=line.split(",");
                 idlist.add(item[0]);
                 namelist.add(item[1]);
                 passwordlist.add(item[2]);
+                typelist.add(item[3]);
             }
         }catch (IOException e) {
             e.printStackTrace();
@@ -108,10 +107,10 @@ public class LoginController {
 
         while (true){// If the password is wrong, it will keep looping
             boolean flag0=password.matches(pattern);
-            if(!flag0){
-                System.out.println("The password is illegal. The" +
-                        " right format: ([0-9](\\w*)){8,20}");
-                return null;
+            if(!flag0){//The password is illegal and throw an exception.
+                MyException myException=new MyException("The password is illegal");
+                throw myException;
+//                return null;
             }
             else {
                 break;
@@ -122,17 +121,20 @@ public class LoginController {
         while(true){
             for(int i=0;i<namelist.size();i++){
                 if((name.equals(namelist.get(i)))&&(password.equals(passwordlist.get(i)))){ // if the user is already a member
-                    System.out.println("You are already a member! Please login in");
+
                     flag=false;
                     break;
                 }
             }
             if (!flag){// user should login in, so return null
-                return null;
+                //throw an exception
+                MyException myException=new MyException("You are already a member");
+                throw myException;
             }
             else {// if the user is a new customer
                 namelist.add(name);
                 passwordlist.add(password);
+                typelist.add(type);
                 try{
                     BufferedWriter writer= new BufferedWriter(new FileWriter(csv,true));//write the info to File login
                     BufferedWriter writer1= new BufferedWriter(new FileWriter(cash,true));// write the info to File cash
@@ -147,12 +149,21 @@ public class LoginController {
                     writer1.close();
                 } catch (IOException e) {
                     e.printStackTrace();
-                    System.out.println("Register failed!");
                     return null;
                 }
-                System.out.println("Register successfully");
-                Customer customer=new Customer(id,name,type,0.00);
-                return customer;
+                if (type.equals("customer")){
+                    Customer customer=new Customer(id,name);
+                    return customer;
+                }
+                else if(type.equals("trainer")){
+                    Trainer trainer=new Trainer(id,name);
+                    return trainer;
+                }
+                else {
+                    MyException myException=new MyException("Type is wrong");
+                    throw myException;
+                }
+
             }
         }
 
