fb3d7bcbffe91f36a71fca1e03e1b76a807117ab
Liquidwork <52688110+Liquidwork@users.noreply.github.com>
2021-04-11 13:46:41 +0800

    refactor. GUIController can work dependently
    GUIController can work dependently
    Several logic changes

--- GUIController.java
+++ GUIController.java
@@ -7,7 +7,8 @@ import javax.swing.JPanel;
  */
 public class GUIController {
     // The stack for store panels, the top of stack is the panel which is currently display
-    private static final Stack<JPanel> PANELS=new Stack<JPanel>();
+    private static Stack<JPanel> PANELS=new Stack<JPanel>();
+    private static JFrame mainFrame;
 
     /**
      * Initialize GUI from the login and register page and push the panel into the stack
@@ -17,37 +18,46 @@ public class GUIController {
      * @seeUser
      */
     public static void Initialize(JFrame frame){
+        mainFrame = frame;
         frame.setTitle("Digital Gym System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setLocationRelativeTo(null);
         frame.setSize(800,500);
         frame.setLocation(400, 100);
         frame.setVisible(true);
-        PANELS.push(new Login(frame).getPanel());
+        switchPage(new Login().getPanel());
     }
 
     /**
      * Switch pages for GUI and push the new page into stack
      * The method set the old panel invisible and the new panel visible
-     * @param from the old panel which will be set invisible
      * @param to the new panel which will be set visible
      * @return void
-     * @seeUser
      */
-    public static void switchPage(JPanel from, JPanel to){
-        from.setVisible(false);
+    public static void switchPage(JPanel to){
+        try {
+            PANELS.peek().setVisible(false);
+        } catch (EmptyStackException e) {
+            // do nothing
+        }
+        mainFrame.add(to);
         to.setVisible(true);
         PANELS.push(to);
+        mainFrame.repaint(); // repaint to avoid bugs in rendering
     }
 
     /**
-     * Switch back to the last page for GUI and pop the current panel from stack
+     * <p>Switch back to the last page for GUI and pop the current panel from stack
      * The method set the current panel invisible and the new panel visible
-     * @param void
-     * @return void
-     * @seeUser
+     * <p>If there is no previous page, the program will exit.
      */
     public static void back(){
-        PANELS.pop().setVisible(false);
-        PANELS.peek().setVisible(true);
+        try {
+            mainFrame.remove(PANELS.pop()); // It will not be found again, so it will be removed
+            PANELS.peek().setVisible(true);
+            mainFrame.repaint(); // repaint to avoid bugs in rendering
+        } catch (EmptyStackException e) {
+            System.exit(0); // Close the program
+        }
     }
 }
