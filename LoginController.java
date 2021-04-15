80c8f2ff504c0113ccf85bc616dede2fe193bbd2
Liquidwork <52688110+Liquidwork@users.noreply.github.com>
2021-04-15 17:18:22 +0800

    refactor. LoginController
    LoginController are now using UserDB to login and register.

--- LoginController.java
+++ LoginController.java
@@ -6,61 +6,25 @@ import exceptions.*;
  * A login controller which provide methods to access user's login data 
  */
 public class LoginController {
-    private static final String loginPathname =  "./data/login.csv"; // file path of login data
-    private static final ArrayList<String> idlist=new ArrayList<>();
-    private static final ArrayList<String> namelist= new ArrayList<String>();
-    private  static  final ArrayList<String> passwordlist= new ArrayList<String>();
-    private static final ArrayList<User.Type> typelist= new ArrayList<>();
 
     /**
      * Login to an account, initiate and return the customer if success, return null if login failed.
      * The method read a file of login data to check for the user's account
      * @param name username to login, name is ignore to case
      * @param password password of login account
-     * @return the user if login success, null if login failed
+     * @return the user if login success, otherwise exceptions will be thrown.
      * @throws PasswordException Password was wrong
      * @throws NoMemberException No such member found
-     * @throws IOException IOException in loading files
-     * @seeUser
      */
-    public static User login(String name, String password) throws PasswordException, NoMemberException, IOException {
-        File csv=new File(loginPathname);
-        BufferedReader br=new BufferedReader(new FileReader(csv));
-        String line=null;
-        int flag=0;
-        int i=1;
-        while ((line= br.readLine())!=null) {//read each line of File login
-            String item[]=line.split(",");
-            item[3]=item[3].substring(0,1).toUpperCase()+item[3].substring(1);
-            User.Type type= User.Type.valueOf(item[3]);
-            typelist.add(type);
-            if((name.equalsIgnoreCase(item[1]))) { //compare the argument name and password with File csv and if the user is a member
-                if (password.equals(item[2])){
-                    //return diffirent types of users
-                    if (type.equals(User.Type.Customer)) {
-                        Customer customer = new Customer(i, item[1]);// create a customer and return it
-                        return customer;
-                    } else if (type.equals(User.Type.Admin)) {
-                        Admin admin = new Admin(i, item[1]);
-                        return admin;
-                    } else {
-                        Trainer trainer = new Trainer(i, item[1]);
-                        return trainer;
-                    }
-                }else {//password is wrong
-                    PasswordException passwordException=new PasswordException("Password is wrong");
-                    throw passwordException;
-                }
-
-            }
-            i+=1;
+    public static User login(String name, String password) throws PasswordException, NoMemberException {
+        User user = UserController.getUserByUsername(name);
+        if (user == null) {
+            throw new NoMemberException("You are not a member yet!");
         }
-        br.close();
-        if(flag==0){// the user is not a member  and throw a exception
-            NoMemberException nomemberException=new NoMemberException("You are not a member");
-            throw nomemberException;
+        if (!UserDB.checkPassword(user, password)){
+            throw new PasswordException("Wrong password");
         }
-        return null;
+        return user;
     }
 
     /**
@@ -71,37 +35,14 @@ public class LoginController {
      * @param name username to login
      * @param password password of login account
      * @param type type to be registered, (admin not advised)
-     * @return the user if login success, null if login failed
      * @throws IllegalException Password or Username illegal, see implNote
      * @throws MemberExistedException Username already taken, which is ignore to cases
-     * @throws IOException IOExceptions in handling files
      * @see User.Type
      * @implNote Password pattern "[A-Za-z0-9]{6,20}", Username pattern "[A-Za-z0-9]{4,20}"
      */
-    public static User register(String name, String password, User.Type type) throws IllegalException, MemberExistedException, IOException {
+    public static void register(String name, String password, User.Type type) throws IllegalException, MemberExistedException {
         if (type == null){
             throw new NullPointerException("type is illegal");
-        }
-        File csv=new File(loginPathname);
-        BufferedReader br=null;
-        try{
-            br=new BufferedReader(new FileReader(csv)); // read each line of File login
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        String line=null;
-        idlist.clear();// clear the arraylist
-        namelist.clear();
-        passwordlist.clear();
-        typelist.clear();
-        while ((line= br.readLine())!=null){
-            String item[]=line.split(",");
-            idlist.add(item[0]);
-            namelist.add(item[1]);
-            passwordlist.add(item[2]);
-            item[3]=item[3].substring(0,1).toUpperCase()+item[3].substring(1);
-            User.Type tpye=User.Type.valueOf(item[3]);
-            typelist.add(tpye);
         }
         String usrPattern="[A-Za-z0-9]{4,20}";
         if(!name.matches(usrPattern)){
@@ -111,37 +52,10 @@ public class LoginController {
         if(!password.matches(pswPattern)){//The password is illegal and throw an exception.
             throw new IllegalException("The password is illegal");
         }
-        int id=1; //id start from 1
-        int lineId, maxId = 0;
-        for(int i=0;i<namelist.size();i++){
-            if((name.equalsIgnoreCase(namelist.get(i)))){ // if the user is already a member
-                MemberExistedException memberException=new MemberExistedException("Username already taken");
-                throw memberException;
-            }
-            lineId = Integer.parseInt(idlist.get(i));
-            if (maxId < lineId){ // Find the max id
-                maxId = lineId;
-            }
+        if (UserController.getUserByUsername(name) != null) {
+            throw new MemberExistedException("This username has been taken.");
         }
-        id = maxId + 1; // id is 1 if no record in login.csv
-        namelist.add(name);
-        passwordlist.add(password);
-        typelist.add(type);
-        BufferedWriter writer= new BufferedWriter(new FileWriter(csv,true));//write the info to File login
-        String newer=id+","+name+","+password+","+type;
-        writer.write(newer);
-        writer.write('\n'); // Newline after recorded
-        writer.close();
-        switch (type){
-            case Customer:
-                return new Customer(id, name);
-            case Trainer:
-                return new Trainer(id, name);
-            case Admin:
-                return new Admin(id, name);
-            default:
-                return null;
-        }
+        UserDB.addUser(name, password, type);
     }
 
     /**
--- UserDB.java
+++ UserDB.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class UserDB{
-    public static ArrayList<User> usersList;
+    private static ArrayList<User> usersList;
+    private static HashMap<User, String> passwordMap;
+    private static int maxId = 0;
     private static final String userPath = "./data/login.csv";
     
     
@@ -13,24 +16,74 @@ public class UserDB{
         return usersList;
     }
 
-    private static void initUsersList() {
+    /**
+     * Check user for this specified {@link User}.
+     * @param user user to be checked
+     * @param password password that user entered
+     * @return true if password matches with data stored
+     */
+    public static boolean checkPassword(User user, String password){
+        if (passwordMap.get(user).equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * <p>Add a new user by indicating its attributes, into both list and file.
+     * <p>This method will not check whether name is unique! Be sure checked before using this method.
+     * @param name
+     * @param password
+     * @param type
+     */
+    public static void addUser(String name, String password, User.Type type){
+        int id = ++maxId;
+        User user;
+        switch (type){
+            case Customer:
+                user = new Customer(id, name);
+                break;
+            case Trainer:
+                user = new Trainer(id, name);
+                break;
+            case Admin:
+                user = new Admin(id, name);
+                break;
+            default:
+                throw new RuntimeException("Not valid user type");
+        }
+        usersList.add(user);
+        passwordMap.put(user, password);
+        DataHandler.append(id + "," + name + "," + password + "," + type, userPath);
+    }
+
+    private static void initUsersList(){
         usersList = new ArrayList<>();
         ArrayList<String> list = DataHandler.read(userPath);
+        passwordMap = new HashMap<>();
         for(String s : list){
             String[] pieces = s.split(",");
+            User user;
+            int id = Integer.parseInt(pieces[0]);
             switch(pieces[3].toLowerCase()){
                 case "customer":
-                    usersList.add(new Customer(Integer.parseInt(pieces[0]), pieces[1])); // Initialize a customer here
-                break;
+                    user = new Customer(id, pieces[1]); // Initialize a customer here
+                    break;
                 case "admin":
-                    usersList.add(new Admin(Integer.parseInt(pieces[0]), pieces[1])); // Initialize an admin here
-                break;
+                    user = new Admin(id, pieces[1]); // Initialize an admin here
+                    break;
                 case "trainer":
-                    usersList.add(new Trainer(Integer.parseInt(pieces[0]), pieces[1])); // Initialize a trainer here
-                break;
+                    user = new Trainer(id, pieces[1]); // Initialize a trainer here
+                    break;
                 default:
-                System.err.println("User type undefined"); // Continue the loop with a error printed on errIO
-                continue;
+                    System.err.println("User type undefined"); // Continue the loop with a error printed on errIO
+                    continue;
+            }
+            usersList.add(user);
+            passwordMap.put(user, pieces[2]);
+            if (maxId < id){
+                maxId = id;
             }
         }
     }
