d6f1ce8dee3a0b964cb7f88c3ba3846658f37477
SiegmeyerG <81668609+SiegmeyerG@users.noreply.github.com>
2021-04-15 19:59:20 +0800

    VideoDB

--- VideoDB.java
+++ VideoDB.java
@@ -1,17 +1,50 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
 public class VideoDB {
    //private static final String basicPath = "D:\\Work Zone\\GitHub\\Digital_gym_system\\data\\video.csv";
-   private static final String Path = "./data/video.csv";
-   private static final String desPath = "./data/video2.csv";
-   private static ArrayList<String> videosList;
-   private static ArrayList<String> descriptions;
+   private static final String dataPath = "./data/video.csv";
+   private static ArrayList<Video> videoList;
 
-   public static void addVideo(int trainerID, String title, String link) {
-        if (videosList == null){
-            videosList = DataHandler.read(Path);
-            descriptions = DataHandler.read(desPath);
+   public static void addVideo(Video video) {
+        if (videoList == null){
+            initVideoList();
+        } 
+        boolean exsited = videoList.removeIf(e ->e.getId()==video.getId());
+        videoList.add(video);
+        if(exsited){
+            ArrayList<String> vList = new ArrayList<>();
+            for(Video s : videoList){
+                String data = s.getId()+","+s.getAuthor().getId()+","+
+                s.getTitle()+","+s.getVideoPath()+","+s.getDescription();
+                vList.add(data);
+            }
+            DataHandler.write(vList, dataPath);
+        }
+        else{
+            String data = video.getId()+","+video.getAuthor().getId()+","+
+            video.getTitle()+","+video.getVideoPath()+","+video.getDescription();
+            DataHandler.append(data, dataPath);
         }
 
+    }
+    private static void initVideoList(){
+        ArrayList<String> videos = DataHandler.read(dataPath);
+        String line;
+        String cvsSplitBy = ",";
+        Iterator<String> iterator = videos.iterator();
+        while (iterator.hasNext()) {
+            line = (String) (iterator.next());
+            String[] ele = line.split(cvsSplitBy);
+            String description = ele[4];
+            for(int i=5; i < ele.length; i++){
+                description = description+ ","+ ele[i]; 
+            }
+            Video data = new Video(ele[0],, ele[2],ele[3], description)
+            videoList.add(data);   
+        }
+    }
+    public VideoDB() {
     }
 }
