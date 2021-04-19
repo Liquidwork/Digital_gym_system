5c46192f7842081fe853e1445cb43fb8e1fdbc11
landeqi-te <57926031+landeqi-te@users.noreply.github.com>
2021-04-20 00:42:50 +0800

    Update LoginController.java

--- LoginController.java
+++ LoginController.java
@@ -1,1 +1,66 @@
+import exceptions.*;
 
+/**
+ * A login controller which provide methods to access user's login data 
+ */
+public class LoginController {
+
+    /**
+     * Login to an account, initiate and return the customer if success, return null if login failed.
+     * The method read a file of login data to check for the user's account
+     * @param name username to login, name is ignore to case
+     * @param password password of login account
+     * @return the user if login success, otherwise exceptions will be thrown.
+     * @throws PasswordException Password was wrong
+     * @throws NoMemberException No such member found
+     */
+    public static User login(String name, String password) throws PasswordException, NoMemberException {
+        User user = UserController.getUserByUsername(name);
+        if (user == null) {
+            throw new NoMemberException("You are not a member yet!");
+        }
+        if (!UserDB.checkPassword(user, password)){
+            throw new PasswordException("Wrong password");
+        }
+        return user;
+    }
+
+    /**
+     * Register an account, initiate and return the user if success, return null if register failed.
+     * The method read a file of login data to check if new account valid, then create a new one to
+     * the end of the file with an auto-allocated id.
+     * The new uid will be the (max(id) + 1).
+     * @param name username to login
+     * @param password password of login account
+     * @param type {@link User.Type} type to be registered, (admin not advised)
+     * @throws IllegalException Password or Username illegal, see implNote
+     * @throws MemberExistedException Username already taken, which is ignore to cases
+     * @implNote Password pattern "[A-Za-z0-9]{6,20}", Username pattern "[A-Za-z0-9]{4,20}"
+     */
+    public static void register(String name, String password, User.Type type) throws IllegalException, MemberExistedException {
+        if (type == null){
+            throw new NullPointerException("type is illegal");
+        }
+        String usrPattern="[A-Za-z0-9]{4,20}";
+        if(!name.matches(usrPattern)){
+            throw new IllegalException("The username is illegal");
+        }
+        String pswPattern="[A-Za-z0-9]{6,20}";// specifies the format of password
+        if(!password.matches(pswPattern)){//The password is illegal and throw an exception.
+            throw new IllegalException("The password is illegal");
+        }
+        if (UserController.getUserByUsername(name) != null) {
+            throw new MemberExistedException("This username has been taken.");
+        }
+        UserDB.addUser(name, password, type);
+    }
+
+    /**
+     * Some test methods.
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println(login("winter", "14530529"));
+        System.out.println(login("luca", "123456"));
+    }
+
+}
