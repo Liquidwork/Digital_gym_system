c171ea7de008a46ea2c4ee11091356a900587f76
Liquidwork <52688110+Liquidwork@users.noreply.github.com>
2021-05-05 20:09:09 +0800

    Update ChatController.java
    removed unused import

--- ChatController.java
+++ ChatController.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 
 import exceptions.NotActiveUserException;
